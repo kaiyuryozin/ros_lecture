009fee708459625cc4fa6b4e3b220448de778e9e
erio <erio@ubuntu>
2018-05-20 01:48:24 -0700

    add vis_lecture

--- basic_lecture/src/simple_talker.cpp
+++ basic_lecture/src/simple_talker.cpp
@@ -2,25 +2,19 @@
 #include "std_msgs/String.h"
 
 int main(int argc, char **argv){
-  ros::init(argc, argv, "talker");
+  ros::init(argc, argv, "simple_talker");
   ros::NodeHandle n;
-  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
+  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 10);
   ros::Rate loop_rate(10);
 
-  int count = 0;
   while (ros::ok()){
     std_msgs::String msg;
-    std::stringstream ss;
-    ss << "hello world " << count;
-    msg.data = ss.str();
-
+    msg.data = "hello world!";
     ROS_INFO("%s", msg.data.c_str());
-
     chatter_pub.publish(msg);
 
     ros::spinOnce();
     loop_rate.sleep();
-    ++count;
   }
   return 0;
 }
